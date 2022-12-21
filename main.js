8a5e1a6a60178f05ce75971159ceb5f7ff9849ac
kavi307 <ajkavitha7@gmail.com>
2022-12-21 10:51:46 +0530

    filter

---
+++ main.js
@@ -0,0 +1,5 @@
+let mobile=[redmi=10000,poco=8000,motorola=2000,nokia=3000,oneplus=20000];
+    let mob=mobile.filter(function(value){
+        return value > 500;
+    });
+        document.write(mob);
